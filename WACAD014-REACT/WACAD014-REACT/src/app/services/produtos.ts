aaa9bbdbb809db6098192e7806d1b21af078f5f0
ricardobraz1 <99514444+ricardobraz1@users.noreply.github.com>
2025-07-26 17:18:08 -0400

    Delete WACAD014-REACT directory

--- WACAD014-REACT/WACAD014-REACT/src/app/services/produtos.ts
+++
@@ -1,7 +0,0 @@
-import { Produto } from "../types/produto";
-import api from "./api"
-
-export async function getListaProduto(): Promise<Produto[]> {
-    return api.get("/produto").then((response)=>response.data);
-    
-}
