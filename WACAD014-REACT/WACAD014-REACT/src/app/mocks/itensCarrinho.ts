3d3fea71fc989cfa0394fd7f374160de08f89533
ricardobraz1 <99514444+ricardobraz1@users.noreply.github.com>
2025-07-29 11:08:05 -0400

    Delete WACAD014-REACT/WACAD014-REACT directory

--- WACAD014-REACT/WACAD014-REACT/src/app/mocks/itensCarrinho.ts
+++
@@ -1,6 +0,0 @@
-import { ItemCarrinho } from "../types/carrinho";
-export const mockItensCarrinho: ItemCarrinho[] = [
-  { id: "notebook-3", nome: "Bicicleta", preco: 1000, quantidade: 2 },
-  { id: "2", nome: "Echo dot", preco: 600, quantidade: 1 },
-  { id: "3", nome: "Celular", preco: 1500, quantidade: 3 },
-];
--- WACAD014-REACT/WACAD014-REACT/src/app/types/carrinho.ts
+++
@@ -1,6 +0,0 @@
-export interface ItemCarrinho {
-  id: string;
-  nome: string;
-  preco: number;
-  quantidade: number;
-}
