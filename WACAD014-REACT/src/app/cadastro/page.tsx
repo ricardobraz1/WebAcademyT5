ccfc423a07ed5dba8c0957614a0bce742a884b50
ricardobraz1 <99514444+ricardobraz1@users.noreply.github.com>
2025-07-30 17:08:56 -0400

    Delete WACAD014-REACT/src/app directory

--- WACAD014-REACT/src/app/cadastro/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-export default function Cadastro() {
-  return (
-    <main className="container mt-5">
-      <h2>Criar Conta</h2>
-      <p>Funcionalidade em construção. Aguarde novidades!</p>
-    </main>
-  )
-}
--- WACAD014-REACT/src/app/services/api.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios"
-
- const produtosApi = axios.create({
-    baseURL: "https://ranekapi.origamid.dev/json/api",
-});
-
-export default produtosApi
