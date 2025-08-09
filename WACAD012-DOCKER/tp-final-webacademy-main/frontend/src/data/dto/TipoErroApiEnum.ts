331d5ff61ed8b3be99811eedb6cec36aaea4c95d
ricardobraz1 <99514444+ricardobraz1@users.noreply.github.com>
2025-08-09 02:11:17 -0400

    Delete WACAD012-DOCKER/tp-final-webacademy-main directory

--- WACAD012-DOCKER/tp-final-webacademy-main/frontend/src/data/dto/TipoErroApiEnum.ts
+++
@@ -1,8 +0,0 @@
-enum TipoErroApiEnum {
-    INTERNO = "Erro interno da aplicação",
-    RESTRICAO = "Erro de restrição de dado",
-    NAO_ENCONTRADO = "Erro de registro não encontrado",
-    VALIDACAO = "Erro de validação de dado"
-}
-
-export { TipoErroApiEnum };
